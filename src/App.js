4982c3b65df85ab75b91da91addf3a524582b1aa
Harrison Fung <harrison.hok.fung@gmail.com>
2020-08-09 11:33:44 -0400

    weather card

--- src/App.js
+++ src/App.js
@@ -26,13 +26,17 @@ const App = () => {
                         <sup>{weather.sys.country}</sup>
                     </h2>
                 <div>
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
+                    {Math.round(weather.main.temp)}
+                    <sup>&deg;C</sup>
                 </div>
+                <div className="info">
+                    <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                    <p>{weather.weather[0].description}</p>
                 </div>
-            )}
-        </div>
-    );
+            </div>
+        )}
+    </div>
+);
 }
 
 export default App;
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const APIKEY = '035458063e7edb1c37197de2e33769dc';
+const APIKEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
